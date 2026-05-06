70adeb0aff148a61b1dd995cf2f1177e01171a21
nikolasfas <n.fasano.polito@gmail.com>
2026-05-06 10:43:36 +0200

    updated 04/05

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,76 @@
 from database.DB_connect import DBConnect
+from model.arco import Arco
+from model.artObject import ArtObject
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllNodes():
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        query = """select * 
+                    from objects o """
+
+        cursor.execute(query)
+        res = []
+        for row in cursor:
+            res.append(ArtObject(**row))
+
+        cursor.close()
+        conn.close()
+        return res
+
+    @staticmethod
+    def getEdgePeso(v1, v2):
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        res = []
+
+        query = """select  eo.object_id as o1, eo2.object_id as o2, count(*) as peso
+                    from exhibition_objects eo , exhibition_objects eo2 
+                    where eo2.exhibition_id = eo.exhibition_id
+                    and eo.object_id < eo2.object_id 
+                    and eo.object_id = %s and eo2.object_id = %s
+                    group by eo.object_id, eo2.object_id  """
+
+        cursor.execute(query, (v1.object_id, v2.object_id))
+
+        for row in cursor:
+            res.append(row["peso"])
+
+        cursor.close()
+        conn.close()
+
+        if len(res) == 0:
+            return None
+        return res
+
+    @staticmethod
+    def getAllEdges(idMapAO):
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        res = []
+
+        query = """select  eo.object_id as o1, eo2.object_id as o2, count(*) as peso
+                    from exhibition_objects eo , exhibition_objects eo2 
+                    where eo2.exhibition_id = eo.exhibition_id
+                    and eo.object_id < eo2.object_id 
+                    group by eo.object_id, eo2.object_id
+                    order by peso desc"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            # res.append((o1, o2, peso))
+            res.append(Arco(idMapAO[row["o1"]], idMapAO[row["o2"]], row["peso"]))
+
+        cursor.close()
+        conn.close()
+
+        if len(res) == 0:
+            return None
+        return res
--- model/model.py
+++ model/model.py
@@ -1,3 +1,60 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
+        self._nodes = DAO.getAllNodes()
+        self._idMapAO = {}
+        for n in self._nodes:
+            self._idMapAO[n.object_id] = n
+
+    def getInfoCompConnessa(self, id_oggetto):
+        # CERCARE COMPONENETE CONNESSA CHE CONTIENE id_oggetto
+        if not self.hasNode(id_oggetto):
+            return None
+        source = self._idMapAO[id_oggetto]
+
+        # 1. FARE UNA RICERCA DFS E CONTARE I NUOVI NODI DELL'ALBERO
+        dfsTree = nx.dfs_tree(self._graph, source)
+        print(f"size connessa con dfs_tree {len(dfsTree)}")
+
+        # 2.
+        dfsPrede = nx.dfs_predecessors(self._graph, source)
+        print(f"size connessa con dfs_predecessors {len(dfsPrede.values())}")
+
+        # 3. UTILIZZARE METODI APPOSITI LIBRERIA
+        conn = nx.node_connected_component(self._graph, source)
+        print(f"size connessa con node_connected_component {len(conn)}")
+        return len(conn)
+
+
+    def hasNode(self, id_oggetto):
+        return id_oggetto in self._idMapAO
+
+    def buildGraph(self):
+        # AGGIUNGE I NODI
+        self._graph.add_nodes_from(self._nodes)
+        # AGGIUNGE GLI ARCHI
+        self.addEdgesV2()
+
+    def addEdges(self):
+        for u in self._nodes:
+            for v in self._nodes:
+                peso = DAO.getEdgePeso(u, v)
+                if peso is not None:
+                    self._graph.add_edge(u, v, weight = peso)
+                    #print(f"Aggiunto arco fra {u} e {v} con peso {peso}")
+
+    def addEdgesV2(self):
+        allEdges = DAO.getAllEdges(self._idMapAO)
+        for e in allEdges:
+            self._graph.add_edge(e.o1, e.o2, weight = e.peso)
+
+    def getNumNodes(self):
+        return len(self._graph.nodes)
+
+    def getNumEdges(self):
+        return len(self._graph.edges)
